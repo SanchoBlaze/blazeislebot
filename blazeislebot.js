998245a938b2001c4092844716807b50bf4d347c
Simon McLaughlin <me@sanchoblaze.com>
2021-04-25 21:18:49 +0100

    Tweaks to package.json and eslint

--- blazeislebot.js
+++ blazeislebot.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * A bot that welcomes new guild members when they join
+ * A bot for the Blaze Isle Discord server.
  */
 
 const config = require('config');
@@ -24,21 +24,14 @@ for (const folder of commandFolders) {
     }
 }
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-// Create an event listener for new guild members
+
 client.on('guildMemberAdd', member => {
-    // Send the message to a designated channel on a server:
     const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
-    // Do nothing if the channel wasn't found on this server
     if (!channel) return;
-    // Send the message, mentioning the member
     const embed = new Discord.MessageEmbed()
         .setTitle(`Welcome to **${member.guild}**`)
         .setDescription(`Hey ${member.user.toString()}, thanks for joining!`)
